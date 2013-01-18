f1c9ebc8e3344877b5056efd46681a03899973d4
Mauricio Sganderla <mauricio.webdev@gmail.com>
2013-01-18 14:08:35 -0200

    more tests

--- test/br/com/trevisan/md5/md5Test.java
+++ test/br/com/trevisan/md5/md5Test.java
@@ -25,5 +25,12 @@ public class md5Test {
 		String encrypted  = "78e731027d8fd50ed642340b7c9a63b3";
 		assertEquals( encrypted, DigestUtils.md5Hex(data));
 	}
+	
+	@Test
+	public void shouldEncryptMD5WithSpecifiedPasswords(){
+		String data = "md5salt#um0v.p4ss#@c3ntu4ção";
+		String encrypted  = "856dceaf0adb371141a669c79f09db24";
+		assertEquals( encrypted, DigestUtils.md5Hex(data));
+	}
 
 }
